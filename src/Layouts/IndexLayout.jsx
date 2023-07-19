6075019e73a1366d7d8763f393e2e51b4e79201f
Juan Rivera <indiojuan2012.jr@gmail.com>
2023-07-19 16:37:00 -0300

    testimonios

--- src/Layouts/IndexLayout.jsx
+++ src/Layouts/IndexLayout.jsx
@@ -6,6 +6,7 @@ import Caracteristicas from '../Components/Caracteristicas/Caracteristicas'
 import Comience from '../Components/Comience/Comience'
 import Transacciones from '../Components/Transacciones/Transacciones'
 import Team from '../Components/Team/Team'
+import Testimonios from '../Components/Testimonios/Testimonios'
 export default function IndexLayout() {
     return (
         <div>
@@ -16,6 +17,7 @@ export default function IndexLayout() {
             <Comience />
             <Transacciones />
             <Team />
+            <Testimonios />
         </div>
     )
 }
